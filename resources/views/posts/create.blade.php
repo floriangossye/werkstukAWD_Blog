244ed3ae55580acd451f3cbb01089f5798d8f8ba
florian.gossye <florian.gossye@student.ehb.be>
2017-12-27 18:41:11 +0100

    Storing to DB Finished + client/server side basic validation.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,19 +1,25 @@
 @extends('main')
 
-@section('title','|Create post')
+@section('title','| Create post')
+@section('stylesheets')
+    {!! Html::style('css/parsley.css') !!}
+@endsection
 @section('content')
 
 <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class=" col-md-8 offset-md-2">
 <h1>Create Post</h1>
         <hr>
-        {!! Form::open(['url' => 'foo/bar']) !!}
+        {!! Form::open(array('route' => 'posts.store','data-parsley-validate'=>'')) !!}
         {{Form::label('title','Title:')}}
-        {{Form::text('title',null,array('class'=>'form-control'))}}
+        {{Form::text('title',null,array('class'=>'form-control','required' => '','maxlength'=>'255'))}}
         {{Form::label('body','Post:')}}
-        {{Form::textarea('body',null,array('class'=>'form-control'))}}
+        {{Form::textarea('body',null,array('class'=>'form-control','required' => ''))}}
         {{Form::submit('Create Post',array('class'=>'btn btn-success btn-lg btn-block','style'=> 'margin-top:20px;'))}}
         {!! Form::close() !!}
     </div>
 </div>
+@endsection
+@section('scripts')
+    {!! Html::script('js/parsley.min.js') !!}
 @endsection
