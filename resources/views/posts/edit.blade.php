ac276e39c1f39d92c973880ade2aefa0b7ac7f38
florian.gossye <florian.gossye@student.ehb.be>
2017-12-28 16:11:42 +0100

    Editing post works + update function to DB

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('main')
+@section('title', '| Edit')
+@section('content')
+    <div class="row">
+        {!! Form::model($post, ['route' => ['posts.update',$post->id], 'method'=>'PUT']) !!}
+        <div class="col-md-12">
+            {{Form::label('title','Title:')}}
+            {{Form::text('title', null, ["class" => 'form-control input-lg'])}}
+
+            {{Form::label('body','Body:',['class'=>'form-spacing-top'])}}
+            {{Form::textarea('body', null,["class"=> 'form-control'])}}
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created at: </dt>
+                    <dd>{{date('j M Y H:i', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Last updated: </dt>
+                    <dd>{{date('j M Y H:i',strtotime($post->updated_at)) }}</dd>
+                </dl>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.show','Cancel', array($post->id),array('class'=>'btn btn-danger btn-block')) !!}
+
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="col-m-6">
+                            {{Form::submit('Save',['class'=>'btn btn-success btn-block' ])}}
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        {!! Form::close()!!}
+    </div>
+@endsection
